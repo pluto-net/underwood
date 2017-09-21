be1d0aae95be1f84b28d621c57cb58545e2bcfb5
am2rican5 <am2rican5@gmail.com>
2017-09-21 10:54:49 +0900

    make token pausable

--- contracts/token/PLTToken.sol
+++ contracts/token/PLTToken.sol
@@ -1,9 +1,13 @@
 pragma solidity ^0.4.15;
 
 import 'zeppelin-solidity/contracts/token/MintableToken.sol';
+import 'zeppelin-solidity/contracts/token/PausableToken.sol';
+import 'zeppelin-solidity/contracts/math/SafeMath.sol';
 
-contract PLTToken is MintableToken {
+contract PLTToken is MintableToken, PausableToken {
+    using SafeMath for uint256;
+
     string public constant name = "Pluto";
     string public constant symbol = "PLT";
-    string public constant decimals = 18;
+    uint8 public constant decimals = 18;
 }
